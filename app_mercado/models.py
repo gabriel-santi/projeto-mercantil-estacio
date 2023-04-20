b994a1ffbe44eb5c22fb56b46772fb0f90fd8adc
GabrielSantiago <marcosgabrielss23@gmail.com>
2023-04-20 19:20:46 -0300

    Criando estrutura HTML inicial

--- app_mercado/models.py
+++ app_mercado/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    prod_desc = models.CharField(max_lenght=200),
+    data_reg = models.DateTimeField('data registro'),
+    valor = models.DecimalField(decimal_places=2),
+    quantidade = models.IntegerField(min_value=0)
+    
+    def __str__(self):
+        return self.choice_text
