4f8d7ecc7578eb02cfb25652cbe8ff37e471dae4
GabrielSantiago <marcosgabrielss23@gmail.com>
2023-04-23 01:27:37 -0300

    Renderizando produtos do banco na index

--- app_mercado/models.py
+++ app_mercado/models.py
@@ -1,4 +1,3 @@
-from datetime import date
 from django.db import models
 
 class Produto(models.Model):
@@ -6,6 +5,9 @@ class Produto(models.Model):
     data_reg = models.DateTimeField('data registro')
     valor = models.DecimalField(decimal_places=2, max_digits=5)
     quantidade = models.IntegerField(default=1)
+
+    def obterValorFormatado(self):
+        return str(self.valor).replace(".",",")
 
     def __str__(self):
         return str(self.prod_desc)
--- app_mercado/views.py
+++ app_mercado/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Produto
 
 def index(request):
-    return render(request, 'home/index.html')
+    lista_produtos = Produto.objects.all()
+    template = loader.get_template('home/index.html')
+    print(lista_produtos)
+    context = {
+        'lista_produtos' : lista_produtos,
+    }
+    return HttpResponse(template.render(context, request))
