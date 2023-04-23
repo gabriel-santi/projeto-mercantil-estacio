a8150bb0d4f6189216bafe41d855a7f2f57cc9ae
GabrielSantiago <marcosgabrielss23@gmail.com>
2023-04-23 00:16:37 -0300

    separando arquivos de css e assets

--- app_mercado/models.py
+++ app_mercado/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Produto(models.Model):
-    prod_desc = models.CharField(max_lenght=200),
+""" class Produto(models.Model):
+    prod_desc = models.CharField(max_length=200),
     data_reg = models.DateTimeField('data registro'),
     valor = models.DecimalField(decimal_places=2),
     quantidade = models.IntegerField(min_value=0)
-    
+
     def __str__(self):
-        return self.choice_text
+        return self.choice_text """
