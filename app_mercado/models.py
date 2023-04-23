bd64126d25d9b5e2440487313c4f06fc93010fdc
GabrielSantiago <marcosgabrielss23@gmail.com>
2023-04-23 00:58:14 -0300

    criando models do banco de dados

--- app_mercado/models.py
+++ app_mercado/models.py
@@ -1,10 +1,11 @@
+from datetime import date
 from django.db import models
 
-""" class Produto(models.Model):
-    prod_desc = models.CharField(max_length=200),
-    data_reg = models.DateTimeField('data registro'),
-    valor = models.DecimalField(decimal_places=2),
-    quantidade = models.IntegerField(min_value=0)
+class Produto(models.Model):
+    prod_desc = models.CharField(max_length=200)
+    data_reg = models.DateTimeField('data registro')
+    valor = models.DecimalField(decimal_places=2, max_digits=5)
+    quantidade = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.choice_text """
+        return str(self.prod_desc)
