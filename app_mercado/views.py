0b31b2251222b20a714b63a3ffe51d6108e94b60
GabrielSantiago <marcosgabrielss23@gmail.com>
2023-04-29 01:38:35 -0300

    Adicionando paginação

--- app_mercado/views.py
+++ app_mercado/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.core.paginator import Paginator
 
 from .models import Produto
 
 def index(request):
     lista_produtos = Produto.objects.all()
-    template = loader.get_template('home/index.html')
-    print(lista_produtos)
-    context = {
-        'lista_produtos' : lista_produtos,
+
+    paginator = Paginator(lista_produtos, 12)
+    page = request.GET.get('pagina')
+    lista_paginada = paginator.get_page(page)
+
+    dados = {
+        'lista_produtos' : lista_paginada,
     }
-    return HttpResponse(template.render(context, request))
+
+    template = loader.get_template('home/index.html')
+
+    return HttpResponse(template.render(dados, request))
